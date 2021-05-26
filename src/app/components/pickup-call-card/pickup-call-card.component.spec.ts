17cbb37b48124a0b0d63bad0af3af0934b862791
Cassio <cassiora08@gmail.com>
2021-05-25 22:45:24 -0300

    Entrega da atividade avaliativa 25/05

---
+++ src/app/components/pickup-call-card/pickup-call-card.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { PickupCallCardComponent } from './pickup-call-card.component';
+
+describe('PickupCallCardComponent', () => {
+  let component: PickupCallCardComponent;
+  let fixture: ComponentFixture<PickupCallCardComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PickupCallCardComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PickupCallCardComponent);
+    component = fixture.componentInstance;
+  }));
+});
